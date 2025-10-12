38dadcb94403252b1d7d76229dbfe64ecd6f83df
mrus7684@gmail.com <misha@Mikhail-MacBook-Pro.local>
2025-10-12 15:34:39 +0300

    MacCommit

--- src/main/java/org/swe/figures/Model/Circle.java
+++ src/main/java/org/swe/figures/Model/Circle.java
@@ -1,4 +1,29 @@
 package org.swe.figures.Model;
 
-public class Circle {
+import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.paint.Color;
+
+public class Circle extends Shape {
+    private double radius;
+
+    public Circle(Color color, double radius) {
+        super(color);
+        this.radius = radius;
+    }
+
+    @Override
+    double area() {
+        return Math.PI * radius * radius;
+    }
+
+    @Override
+    public void draw(GraphicsContext gr) {
+
+    }
+
+    @Override
+    public void draw(GraphicsContext graphicsContext, double x, double y) {
+        graphicsContext.setStroke(super.color);
+        graphicsContext.strokeOval(x, y, radius, radius);
+    }
 }
